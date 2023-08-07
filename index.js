82a28364d11459ef207b1ae3bb999e23ed297b3b
mamtha <mamatha.amarawadi2002@gmail.com>
2023-08-07 13:25:06 +0000

    react2

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  // Write your code here.
+  <div className="container">
+    <h1>Congratulations</h1>
+
+    <div className="profile">
+      <div>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+        <h1>Kiran V</h1>
+      </div>
+      <p>Vishnu Institute Of Computer Education and Technology,Bhimavaram </p>
+      <div>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
